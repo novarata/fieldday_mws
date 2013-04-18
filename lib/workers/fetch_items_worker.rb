186184b6e1b3ae7cba15a55abfefaddc8117ceb7
Edward Wible <aewible@gmail.com>
2013-04-18 15:52:50 -0400

    add throttling to orders queue

--- lib/workers/fetch_items_worker.rb
+++ lib/workers/fetch_items_worker.rb
@@ -1,6 +1,6 @@
 class FetchItemsWorker
   include Sidekiq::Worker
-  sidekiq_options retry: false, queue: :order_items, throttle: { threshold: 1, period: 6.seconds }
+  sidekiq_options queue: :order_items, throttle: { threshold: 1, period: 6.seconds }
 
   def perform(store_id, order_id, amazon_order_id, parent_request_id=nil)
     ApiRequest.fetch_items(store_id, order_id, amazon_order_id, parent_request_id)
--- lib/workers/fetch_orders_worker.rb
+++ lib/workers/fetch_orders_worker.rb
@@ -1,6 +1,6 @@
 class FetchOrdersWorker
   include Sidekiq::Worker
-  sidekiq_options :retry => false, :queue => :orders
+  sidekiq_options queue: :orders, throttle: { threshold: 1, period: 6.seconds }
 
   def perform(store_id, time_from=nil, time_to=nil)
     ApiRequest.fetch_orders(store_id, time_from, time_to)
